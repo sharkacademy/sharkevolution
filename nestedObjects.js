7c8df1a17a32ab55015cb81e44c3748c756fd5b9
Maria Carolina <maria.amaral@aoop.com.br>
2026-03-26 17:19:51 -0300

    update

--- nestedObjects.js
+++ nestedObjects.js
@@ -8,7 +8,7 @@ let company = {
 
 console.log(company.name);
 company.employees.push({ name: "Peter", position: "Consultant" });
-let employeeNames = company.employees.map(obj => obj.name);
+let employeeNames = company.employees.map(obj => obj.name.trim().toLowerCase());
 console.log(company.employees);
 
-[{ name: "Maria" }, { name: "Luiz" }]
+// [{ name: "Maria" }, { name: "Luiz" }]
